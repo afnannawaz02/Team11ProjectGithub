06aaab0989577ae3c8f37551f0caa4fd44b7da0a
afnannawaz02 <afnan.nawaz@ibm.com>
2026-07-17 15:13:35 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2379,13 +2379,8 @@ function ChatView({ profile, username }) {
           size="sm"
           className="chat-history-new-btn"
           onClick={newChat}
-          renderIcon={() => (
-            <svg viewBox="0 0 16 16" fill="none" width="14" height="14" xmlns="http://www.w3.org/2000/svg">
-              <path d="M8 1v14M1 8h14" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
-            </svg>
-          )}
         >
-          New chat
+          + New chat
         </Button>
 
         <div className="chat-history-list">
